67903263f15987c3ddf11da5e1c841f7387a7f00
William Cameron <gnomorian@gmail.com>
2023-10-02 20:44:47 +1300

    saves game and creates basic stat file on victory

--- Patches.cs
+++ Patches.cs
@@ -1,22 +1,60 @@
 ﻿using HarmonyLib;
+using I2.Loc.SimpleJSON;
 using System;
 using System.CodeDom;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 using UnityEngine;
+
 
 namespace PNEScoreboard
 {
     [HarmonyPatch]
     public partial class Plugin
     {
-        static BuildingBar buildingBar;
         [HarmonyPatch]
-        public static class MoveBazzarrSideBar
+        public static class MapGameplayPatch
         {
+            private static string CreateVictorySaveGame(MapGameplay __instance)
+            {
+                // the pause game, ui state stuff copied from void QuickSave() in MapGameplay class.
+                var timeManager = Traverse.Create(__instance).Field("_timeManager").GetValue<TimeManager>();
+                var uiManager = Traverse.Create(__instance).Field("_uiManager").GetValue<UIManager>();
+
+                bool isPaused = timeManager.IsPaused;
+                timeManager.PauseGame(isPaused: true);
+                uiManager.StackObscurationState();
+
+                // now actually save the game
+                string savePath = FileManager.GetSavePath($"Victory_{__instance.CurrentFamily.Name}_{__instance.Level.MapName}_{DateTime.Now.ToString("yyyyMMdd")}");
+                LOG.LogWarning($"Saving the game as  \"{savePath}\"");
+                GlobalAccessor.Level.SaveMap(savePath);
+
+                uiManager.ApplyLastObscurationState();
+                timeManager.PauseGame(isPaused);
+
+                return savePath;
+            }
+
+            private static void CreateVictoryStatsFile(MapGameplay __instance, string path)
+            {
+                var stats = new VictoryStatistics(__instance);
+                var serializer = new XmlSerializer(typeof(VictoryStatistics));
+
+                var filePath = Path.ChangeExtension(path, ".xml");
+                LOG.LogWarning($"Saving victory statistics to {filePath}");
+                TextWriter writer = new StreamWriter(filePath);
+                serializer.Serialize(writer, stats);
+                writer.Close();
+                //TODO: confic option to not open the save folder.
+                System.Diagnostics.Process.Start("explorer.exe", Path.GetDirectoryName(filePath));
+            }
+
             [HarmonyPrepare]
             public static void Prepare(MethodBase original)
             {
@@ -25,34 +63,19 @@ namespace PNEScoreboard
                     LOG.LogWarning($"Preparing patch in {original.Name}!");
                 }
             }
-        }
-        [HarmonyTargetMethods]
-        public static IEnumerable<MethodBase> TargetMethods()
-        {
-            yield return typeof(BuildingBar).GetMethod("RefreshAvailableCategories");
-        }
-
-        [HarmonyPrefix]
-        public static void Prefix(BuildingBar __instance)
-        {
-            LOG.LogWarning($"The BuildingBar instance = {__instance.name}");
-            buildingBar = __instance;
-        }
-
-        [HarmonyPostfix]
-        public static void Postfix()
-        {
-            if (buildingBar == null)
+            [HarmonyTargetMethods]
+            public static IEnumerable<MethodBase> TargetMethods()
             {
-                return;
+                yield return typeof(MapGameplay).GetMethod("ComputeVictoryData");
             }
-            var catagoryHolder = Traverse.Create(buildingBar).Field("_categoryHolder").GetValue<Transform>();
 
-            LOG.LogWarning($"The catagoryHolder {catagoryHolder.name} has {catagoryHolder.childCount} children.");
-            for(int i = 0; i < catagoryHolder.childCount; i++) 
+            [HarmonyPrefix]
+            public static void Prefix(MapGameplay __instance)
             {
-                LOG.LogWarning($"\t{catagoryHolder.GetChild(i).name}");
+                var savePath = CreateVictorySaveGame(__instance);
+                CreateVictoryStatsFile(__instance, savePath);
             }
         }
+
     }
 }
---
+++ VictoryStatistics.cs
@@ -0,0 +1,48 @@
+﻿using HarmonyLib;
+using I2.Loc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PNEScoreboard
+{
+    public class VictoryStatistics
+    {
+        public int MissionDuration;
+
+        public string Difficulty;
+
+        public int CultureRating;
+
+        public int ProsperityRating;
+
+        public int KingdomRating;
+
+        public int CityFunds;
+
+        public int Population;
+
+        public int HouseQuantity;
+
+        public int HouseLevel;
+
+        public VictoryStatistics() { }
+
+        public VictoryStatistics(MapGameplay mapGameplay) 
+        {
+            Population = Traverse.Create(mapGameplay).Field("_populationManager").GetValue<PopulationManager>().TotalPopulation;
+            CityFunds = mapGameplay.Treasury;
+            KingdomRating = mapGameplay.KingdomRating;
+            CultureRating = mapGameplay.GetCultureRating();
+            ProsperityRating = mapGameplay.MonthlyProsperityRating;
+            var timeManager = Traverse.Create(mapGameplay).Field("_timeManager").GetValue<TimeManager>();
+            MissionDuration = timeManager.YearsElapsed * 12 + timeManager.CurrentMonth + 1;
+            var level = Traverse.Create(mapGameplay).Field("_level").GetValue<LevelMap>();
+            Difficulty = LocalizationManager.GetTranslation($"Menu/Options/#Difficulty_{level.LowestDifficulty}");
+            HouseLevel = level.WinCondition.HouseLevel;
+            HouseQuantity = level.GetStoredBuildings<House>().Count((House h) => (int)h.CurrentHouseLevel >= level.WinCondition.HouseLevel);
+        }
+    }
+}
